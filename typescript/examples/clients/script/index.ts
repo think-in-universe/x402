bb6a5b26f28ec21274404eb15c923469adf8abea
CarsonRoscoe <carsonroscoe7@gmail.com>
2025-04-10 10:09:44 -0400

    feat: client script example using axios

---
+++ typescript/examples/clients/script/index.ts
@@ -0,0 +1,39 @@
+import 'dotenv/config';
+
+import { createWalletClient, http } from 'viem';
+import { privateKeyToAccount } from 'viem/accounts';
+import { withPaymentInterceptor } from "x402-axios"
+import axios from "axios"
+
+const {
+  RESOURCE_SERVER_URL,
+  PRIVATE_KEY,
+  ENDPOINT_PATH,
+} = process.env;
+
+if (
+  !RESOURCE_SERVER_URL ||
+  !PRIVATE_KEY ||
+  !ENDPOINT_PATH
+) {
+  console.error('Missing required environment variables');
+  process.exit(1);
+}
+
+const account = privateKeyToAccount(PRIVATE_KEY as `0x${string}`);
+const client = createWalletClient({
+  account,
+  transport: http(),
+});
+
+const api = axios.create({
+  baseURL: `${RESOURCE_SERVER_URL}`
+})
+
+withPaymentInterceptor(api, client)
+
+api.get(`${ENDPOINT_PATH}`).then(res => {
+  console.log(res.data)
+}).catch(err => {
+  console.log(err.response.data)
+})
