abb0936d13d3cb80916ec70224c7f5977757cd86
CarsonRoscoe <carsonroscoe7@gmail.com>
2025-04-10 17:53:26 -0400

    feat: added x402-fetch package and fetch client example

---
+++ typescript/examples/clients/fetch/index.ts
@@ -0,0 +1,40 @@
+import { config } from 'dotenv';
+import { createWalletClient, http, publicActions } from 'viem';
+import { privateKeyToAccount } from 'viem/accounts';
+import { fetchWithPayment } from "x402-fetch"
+import { baseSepolia } from 'viem/chains';
+
+config();
+
+const {
+  RESOURCE_SERVER_URL,
+  PRIVATE_KEY,
+  ENDPOINT_PATH,
+} = process.env;
+
+if (
+  !RESOURCE_SERVER_URL ||
+  !PRIVATE_KEY ||
+  !ENDPOINT_PATH
+) {
+  console.error('Missing required environment variables');
+  process.exit(1);
+}
+
+const account = privateKeyToAccount(PRIVATE_KEY as `0x${string}`);
+const client = createWalletClient({
+  account,
+  transport: http(),
+  chain: baseSepolia
+}).extend(publicActions);
+
+const fetch2 = fetchWithPayment(fetch, client)
+
+fetch2(`${RESOURCE_SERVER_URL}${ENDPOINT_PATH}`, {
+  method: "GET",
+}).then(async response => {
+  const body = await response.json()
+  console.log(body)
+}).catch(error => {
+  console.error(error.response?.data?.error)
+})
