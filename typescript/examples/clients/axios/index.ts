9c2fee9d545e6eda64f3287b436e214fe58fc9a7
CarsonRoscoe <carsonroscoe7@gmail.com>
2025-04-11 16:55:02 -0400

    fix: format/lint example axios client

--- typescript/examples/clients/axios/index.ts
+++ typescript/examples/clients/axios/index.ts
@@ -1,24 +1,16 @@
-import { config } from 'dotenv';
-import { createWalletClient, http, publicActions } from 'viem';
-import { privateKeyToAccount } from 'viem/accounts';
-import { withPaymentInterceptor } from "x402-axios"
-import axios from "axios"
-import { baseSepolia } from 'viem/chains';
+import { config } from "dotenv";
+import { createWalletClient, http, publicActions } from "viem";
+import { privateKeyToAccount } from "viem/accounts";
+import { withPaymentInterceptor } from "x402-axios";
+import axios from "axios";
+import { baseSepolia } from "viem/chains";
 
 config();
 
-const {
-  RESOURCE_SERVER_URL,
-  PRIVATE_KEY,
-  ENDPOINT_PATH,
-} = process.env;
+const { RESOURCE_SERVER_URL, PRIVATE_KEY, ENDPOINT_PATH } = process.env;
 
-if (
-  !RESOURCE_SERVER_URL ||
-  !PRIVATE_KEY ||
-  !ENDPOINT_PATH
-) {
-  console.error('Missing required environment variables');
+if (!RESOURCE_SERVER_URL || !PRIVATE_KEY || !ENDPOINT_PATH) {
+  console.error("Missing required environment variables");
   process.exit(1);
 }
 
@@ -26,16 +18,22 @@ const account = privateKeyToAccount(PRIVATE_KEY as `0x${string}`);
 const client = createWalletClient({
   account,
   transport: http(),
-  chain: baseSepolia
+  chain: baseSepolia,
 }).extend(publicActions);
 
-const api = withPaymentInterceptor(axios.create({
-  baseURL: `${RESOURCE_SERVER_URL}`
-}), client)
+const api = withPaymentInterceptor(
+  axios.create({
+    baseURL: `${RESOURCE_SERVER_URL}`,
+  }),
+  client,
+);
 
-api.get(`${ENDPOINT_PATH}`).then(response => {
-  console.log(response.headers)
-  console.log(response.data)
-}).catch(error => {
-  console.error(error.response?.data?.error)
-})
+api
+  .get(`${ENDPOINT_PATH}`)
+  .then(response => {
+    console.log(response.headers);
+    console.log(response.data);
+  })
+  .catch(error => {
+    console.error(error.response?.data?.error);
+  });
