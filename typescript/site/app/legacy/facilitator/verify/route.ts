db9113dc4530b5f34feb9b6d6811ca30cb807632
CarsonRoscoe <carsonroscoe7@gmail.com>
2025-04-24 10:13:51 -0400

    fix: remove unused ts-expect-error

--- typescript/site/app/legacy/facilitator/verify/route.ts
+++ typescript/site/app/legacy/facilitator/verify/route.ts
@@ -31,7 +31,6 @@ export async function POST(req: Request) {
 
   const paymentDetails = paymentDetailsSchema.parse(body.details);
 
-  // @ts-expect-error infinite instantiation
   const valid = await verify(client, body.payload, paymentDetails);
 
   return Response.json(valid);
